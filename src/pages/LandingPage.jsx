419523ba9091b48e227be135ef31cafce223cd9a
Victor Yohanna <victoryohanna@gmail.com>
2023-01-28 18:50:54 +0100

    creates landingpage component

---
+++ src/pages/LandingPage.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react';
+
+class LandingPage extends React.Component{
+    render(){
+        return(
+            <div className=''>
+                
+            </div>
+        )
+    }
+} 
+
+export default LandingPage;
