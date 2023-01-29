5252439e93139bfefea109df9a209f0b723e39a6
Victor Yohanna <victoryohanna@gmail.com>
2023-01-29 18:43:29 +0100

    modified header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,8 @@
 import "../styles/components/header.css";
 import { FaTwitter, FaAndroid } from "react-icons/fa";
+import {RxEyeOpen} from 'react-icons/rx';
+import {SiVuetify} from 'react-icons/si'
+import {HiArrowLongRight, HiArrowLongLeft} from 'react-icons/hi'
 
 const Header = () => {
   return (
@@ -23,7 +26,28 @@ const Header = () => {
         <span className="nfts-total">256,000,000 NFTs</span>
       </div>
       <div className="box-2">
-        <img src="" alt="nft" />
+        <div className="img-section">
+          <div className="img-container">
+          <img src="" alt="nft" />
+          </div>
+          
+          <div className="info-views">
+            <div className="rx-eye"><RxEyeOpen/>( 450K)</div>
+            <div className="si-vue"><SiVuetify/> 0.908 ETH</div>
+          </div>
+          <div className="img-text">
+            <h3>Project Woof</h3>
+            <p>The world's first NFT for dogs. Created as a medium for dogs
+              to feel human wothout the pressure of society
+            </p>
+          </div>
+        </div>
+        <div className="nav-section">
+          <div className="left-arrow">
+            
+          </div>
+        </div>
+        
       </div>
     </div>
   );
