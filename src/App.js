ed8db167d0b2cdb14aab936c89c99aed9ed49e1f
Victor Yohanna <victoryohanna@gmail.com>
2023-01-29 04:11:38 +0100

    modified App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
-//import './App.css';
+import './App.css';
 import LandingPage from "./pages/LandingPage";
+import NavBar from './components/NavBar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <LandingPage/>
+      <Footer/>
     </div>
   );
 }
