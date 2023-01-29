81c5715807cc9ac0b56b7f6647706a351a1e33c2
Victor Yohanna <victoryohanna@gmail.com>
2023-01-29 08:26:25 +0100

    modified and styled header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,25 @@
 
+import '../styles/components/header.css'
 const Header = ()=>{
     return(
-        <div className="header-contaner">
-            <h1>Header</h1>
+        <div className="header-container">
+            <div className="box-1">
+                <div className="social-media"></div>
+                <div className="text-section">
+                    <p>
+                        Discover, collect, create & sell extraodinary NFTs
+                    </p>
+                </div>
+                <div className="button-section">
+                <button className="explore">Explore</button>
+                <button className="create">Create</button>
+                
+                </div>
+                <span>text</span>
+            </div>
+            <div className="box-2">
+                <img src="" alt="nft" />
+            </div>
         </div>
     )
 }
