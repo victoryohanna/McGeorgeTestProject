dd19d9cc994c7b9ebdc6cbe9ff3129c0e36c98ea
Victor Yohanna <victoryohanna@gmail.com>
2023-01-30 09:27:22 +0100

    modified hottest bids component

--- src/components/HottestBids.jsx
+++ src/components/HottestBids.jsx
@@ -1,10 +1,28 @@
+import "../styles/components/hottestBids.css";
 
-const Hottest = ()=>{
-    return(
-        <div className="hottest-container">
-            <h1>Hottest Bids</h1>
+const Hottest = () => {
+  return (
+    <div className="hottest-container">
+      <div className="info-title">
+        <div className="info-text">
+          <h1>Hottest Bids</h1>
         </div>
-    )
-}
+        <div className="info-timer">
+          <input type="text" />
+        </div>
+      </div>
+      <div className="info-body">
+        <div className="hottestbid">
+          <div className="bids">
+            <div className="averta">Averta</div>
+          </div>
+          <div className="bids">
+            <div className="averta">Averta</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Hottest;
