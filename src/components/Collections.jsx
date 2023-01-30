5800082c72b6e158272a7969c81136b329a13011
Victor Yohanna <victoryohanna@gmail.com>
2023-01-30 08:47:22 +0100

    modified collection component

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -1,10 +1,33 @@
+import "../styles/components/collections.css";
 
-const Collections = ()=>{
-    return(
-        <div className='collection-container'>
-            <h1>Collections</h1>
+const Collections = () => {
+  return (
+    <div className="collection-container">
+      <div className="top-collections">
+        <div>
+          <h1>Top Collections</h1>
         </div>
-    )
-}
+        <span>24hrs Volume</span>
+        <div className="collections">
+          <div className="averta">Averta</div>
+          <soan>Item Name</soan>
+        </div>
+
+        <button className="btn-explore">Explore</button>
+      </div>
+      <div className="top-sellers">
+        <div>
+          <h1>Top Sellers</h1>
+        </div>
+        <span>Ranking</span>
+        <div className="sellers">
+          <div className="averta">Averta</div>
+        </div>
+
+        <button className="btn-discover">Discover</button>
+      </div>
+    </div>
+  );
+};
 
 export default Collections;
