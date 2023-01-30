93846724072425723f6d78978dd413f3a8c27d32
Victor Yohanna <victoryohanna@gmail.com>
2023-01-30 17:07:21 +0100

    modified trending component

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,30 +1,34 @@
 
 import "../styles/components/trending.css";
-const Trending = ()=>{
-    return(
-        <div className="trending-container">
-            <div className="trend-caption">
-                <div className="caption-text">
-                    <h2 >Trending</h2>
-                    <button className="btn-view">View All</button>
-                </div>
-                <div className="trend-img">
-                    <div className="img1"></div>
-                    <div className="img2"></div>
-                    <div className="img3"></div>
-                    <div className="img4"></div>
-                    <div className="img5"></div>
-                </div>
-                <div className="trend-img">
-                    <div className="img1"></div>
-                    <div className="img2"></div>
-                    <div className="img3"></div>
-                    <div className="img4"></div>
-                    <div className="img5"></div>
-                </div>
-            </div>
+
+const Trending = () => {
+  return (
+    <div className="trending-container">
+      <div className="trend-caption">
+        <div className="trend-text">
+          <h2>Trending</h2>
         </div>
-    )
-}
+        <div className="trend-button">
+          <button type="button">View All</button>
+        </div>
+      </div>
+
+      <div className="trend-img1">
+        <div className="img1"></div>
+        <div className="img2"></div>
+        <div className="img3"></div>
+        <div className="img4"></div>
+        <div className="img5"></div>
+      </div>
+      <div className="trend-img2">
+        <div className="img1"></div>
+        <div className="img2"></div>
+        <div className="img3"></div>
+        <div className="img4"></div>
+        <div className="img5"></div>
+      </div>
+    </div>
+  );
+};
 
 export default Trending;
