e1340346c2edfed3b5355a85ea4d7f587d0c55a0
Victor Yohanna <victoryohanna@gmail.com>
2023-01-30 07:46:21 +0100

    modified trending component

--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,8 +1,28 @@
 
+import "../styles/components/trending.css";
 const Trending = ()=>{
     return(
         <div className="trending-container">
-            <h1>Trendings</h1>
+            <div className="trend-caption">
+                <div className="caption-text">
+                    <h2 >Trending</h2>
+                    <button className="btn-view">View All</button>
+                </div>
+                <div className="trend-img">
+                    <div className="img1"></div>
+                    <div className="img2"></div>
+                    <div className="img3"></div>
+                    <div className="img4"></div>
+                    <div className="img5"></div>
+                </div>
+                <div className="trend-img">
+                    <div className="img1"></div>
+                    <div className="img2"></div>
+                    <div className="img3"></div>
+                    <div className="img4"></div>
+                    <div className="img5"></div>
+                </div>
+            </div>
         </div>
     )
 }
