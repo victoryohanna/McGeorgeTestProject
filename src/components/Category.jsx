f89d9bfb265eaedd249552af67f2ab34cfc56c23
Victor Yohanna <victoryohanna@gmail.com>
2023-01-30 15:03:30 +0100

    modified and styled category component

--- src/components/Category.jsx
+++ src/components/Category.jsx
@@ -1,8 +1,46 @@
 
+import '../styles/components/category.css';
+
 const Categories = ()=>{
     return(
         <div className="category-container">
-            <h1>Category</h1>
+            <div className="category-title">
+                <h1>Browse by Category</h1>
+            </div>
+            <div className="category">
+                <div className="items-category">
+                    <div className="image-caategory"></div>
+                    <div className='sub-title'><h2>Collectibles</h2></div>
+                    <div className="description">
+                        <p>Lost money holding shib? project shiba is a
+                            way to stand in solidarity with others
+                        </p>
+                    </div>
+                </div>
+
+                <div className="items-category">
+                    <div className="image-caategory"></div>
+                    <div className='sub-title'><h2>Arts</h2></div>
+                    <div className="description">
+                        <p>Lost money holding shib? project shiba is a
+                            way to stand in solidarity with others
+                        </p>
+                    </div>
+                </div>
+
+                <div className="items-category">
+                    <div className="image-caategory"></div>
+                    <div className='sub-title'><h2>Photography</h2></div>
+                    <div className="description">
+                        <p>Lost money holding shib? project shiba is a
+                            way to stand in solidarity with others
+                        </p>
+                    </div>
+                </div>
+            </div>
+            <div className="btn-category">
+                <button type='button'>All Categories</button>
+            </div>
         </div>
     )
 }
