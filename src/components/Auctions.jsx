4ee27cf311b07411fd1d8abbb91275d3a880b23c
Victor Yohanna <victoryohanna@gmail.com>
2023-01-30 08:58:45 +0100

    modified auction component

--- src/components/Auctions.jsx
+++ src/components/Auctions.jsx
@@ -1,8 +1,29 @@
 
+import '../styles/components/auctions.css';
+
 const Auctions = ()=>{
     return(
         <div className="auctions-container">
-            <h1>Auctions</h1>
+            <div className="trend-caption">
+                <div className="caption-text">
+                    <h2 >Live Auctions</h2>
+                    <button className="btn-view">View All</button>
+                </div>
+                <div className="trend-img">
+                    <div className="img1"></div>
+                    <div className="img2"></div>
+                    <div className="img3"></div>
+                    <div className="img4"></div>
+                    <div className="img5"></div>
+                </div>
+                <div className="trend-img">
+                    <div className="img1"></div>
+                    <div className="img2"></div>
+                    <div className="img3"></div>
+                    <div className="img4"></div>
+                    <div className="img5"></div>
+                </div>
+            </div>
         </div>
     )
 }
