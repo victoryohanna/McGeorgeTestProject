cbb6cc8d64a377c2ef882f856d9b2aff30520d01
Victor Yohanna <victoryohanna@gmail.com>
2023-01-31 10:24:10 +0100

    modified footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,74 @@
+import { Link } from "react-router-dom";
+import {
+  FaTwitter,
+  FaLinkedin,
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+} from "react-icons/fa";
+import "../styles/components/footer.css";
 
-const Footer = ()=>{
-    return(
-        <div className="footer-container">
-            <h1>Footer</h1>
+const Footer = () => {
+  return (
+    <div className="footer-container">
+      <div className="footer-wrapper">
+        <div className="search-info">
+          <div className="search">
+            <div className="logo-section">
+              <div className="logo"></div>
+              <h2>DStage</h2>
+            </div>
+            <p>Get all the latest updates regarding NFTs</p>
+            <div className="search-bar">
+              <form>
+                <input type="text" />
+                <button type="button">Search</button>
+              </form>
+            </div>
+          </div>
+          <div className="quick-links">
+            <h5>Quick Links</h5>
+            <ul>
+              <li>
+                <Link to="/about">About Us</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <li>
+                <Link to="/privacy">Privacy Policy</Link>
+              </li>
+              <li>
+                <Link to="/terms">Terms of Use</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="copyright-section">
+        <div className="copyright">
+          <span>&copy;2022 DStage All rights reserved</span>
+        </div>
+        <div className="social-links">
+          <span>
+            <FaTwitter />
+          </span>
+          <span>
+            <FaLinkedin />
+          </span>
+          <span>
+            <FaFacebook />
+          </span>
+          <span>
+            <FaInstagram />
+          </span>
+          <span>
+            <FaYoutube />
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
