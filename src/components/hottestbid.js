2f73a7b47d8d658c4139bc1767adb0bf5e17cbc7
Victor Yohanna <victoryohanna@gmail.com>
2023-01-31 22:51:22 +0100

    creates hottestbid js file

---
+++ src/components/hottestbid.js
@@ -0,0 +1,55 @@
+
+const NFTsHottestBids = [
+    {
+        name: "CrowdSufers",
+        views: "(405K)",
+        value : "0.98 ETH"
+    },
+    {
+        name: "MKRS",
+        views: "(222K)",
+        value : "0.325 ETH"
+    },
+    {
+        name: "Proof-of-Change",
+        views: "(35K)",
+        value : "0.908 ETH"
+    },
+    {
+        name: "BigFoot Town",
+        views: "(666K)",
+        value : "0.309 ETH"
+    },
+    {
+        name: "Reugged Revenants Club",
+        views: "(200K)",
+        value : "1.43 ETH"
+    },
+    {
+        name: "NFTLeaguez",
+        views: "(1.13M)",
+        value : "1.34 ETH"
+    },
+    {
+        name: "Lonely Listeners",
+        views: "(191K)",
+        value : "0.508 ETH"
+    },
+    {
+        name: "Infinite Drive - Car Race",
+        views: "(5K)",
+        value : "23.34 ETH"
+    },
+    {
+        name: "Women of Mars",
+        views: "(176K)",
+        value : "0.45 ETH"
+    },
+    {
+        name: "Wolfies",
+        views: "(409K)",
+        value : "1.234 ETH"
+    },
+]
+
+export default NFTsHottestBids;
