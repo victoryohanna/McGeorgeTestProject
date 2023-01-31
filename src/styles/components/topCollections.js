3f5d6eddfbac34a91e094481977d7dcc73f3c63f
Victor Yohanna <victoryohanna@gmail.com>
2023-01-31 15:01:31 +0100

    added topsellers and topcollections file

---
+++ src/styles/components/topCollections.js
@@ -0,0 +1,55 @@
+
+const NFTsToCollections = [
+    {
+        image: "",
+        name: "Onigiri Pepes",
+        value: "1654.45ETH",
+        qnty: 0.20,
+        figure: "+81.93"
+    },
+    {
+        image: "",
+        name: "Mutant Ape Yatch Club",
+        value: "4567.45ETH",
+        qnty: 13.50,
+        figure: "+2"
+    },
+    {
+        image: "",
+        name: "BliensNFT",
+        value: "2344.55ETH",
+        qnty: 0.65,
+        figure: "+101.22"
+    },
+    {
+        image: "",
+        name: "Rare Apepe YC",
+        value: "34.34ETH",
+        qnty: 12.30,
+        figure: "+55.56"
+    },
+    {
+        image: "",
+        name: "moodlies by Hanuka",
+        value: "5432.34ETH",
+        qnty: 0.40,
+        figure: "+81.93"
+    },
+    {
+        image: "",
+        name: "DigiDaigaku",
+        value: "9845.22ETH",
+        qnty: 1.20,
+        figure: "+97.89"
+    },
+    {
+        image: "",
+        name: "CLONEX-XTAKASHI...",
+        value: "123.34ETH",
+        qnty: 55.55,
+        figure: "-45.56"
+    },
+    
+]
+
+export default NFTsCollections;
---
+++ src/styles/components/topSellers.js
@@ -0,0 +1,46 @@
+const NFTsTopSellers = [
+  {
+    image: "",
+    name: "OxdO5e42nhsw9",
+    value: "0.98ETH",
+    status: "31k NFTs Sold",
+  },
+  {
+    image: "",
+    name: "Ella Mae",
+    value: "0.98ETH",
+    status: "53k NFTs Sold",
+  },
+  {
+    image: "",
+    name: "2j46hai...",
+    value: "0.98ETH",
+    status: "300k NFTs Sold",
+  },
+  {
+    image: "",
+    name: "Elena Kyle",
+    value: "0.98ETH",
+    status: "3.1k NFTs Sold",
+  },
+  {
+    image: "",
+    name: "Amandah Adams",
+    value: "0.98ETH",
+    status: "3.13k NFTs Sold",
+  },
+  {
+    image: "",
+    name: "Oa8bh2nk...",
+    value: "0.98ETH",
+    status: "3.13k NFTs Sold",
+  },
+  {
+    image: "",
+    name: "Mae Jackson",
+    value: "0.98ETH",
+    status: "3.13k NFTs Sold",
+  },
+];
+
+export default NFTsTopSellers;
