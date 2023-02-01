dfbf3e05a716d57ca6075741ca748b87a9ee8d1f
Victor Yohanna <victoryohanna@gmail.com>
2023-02-01 06:44:54 +0100

    modified hottestbid js file

--- src/components/hottestbid.js
+++ src/components/hottestbid.js
@@ -1,51 +1,74 @@
 
+import {
+    ellipse1,
+    ellipse2,
+    ellipse3,
+    ellipse4,
+    ellipse5,
+    ellipse6,
+    ellipse7,
+    ellipse8,
+    ellipse9,
+    ellipse10,
+  } from "./index";
+
 const NFTsHottestBids = [
     {
+        averta: ellipse1,
         name: "CrowdSufers",
         views: "(405K)",
         value : "0.98 ETH"
     },
     {
+        averta: ellipse2,
         name: "MKRS",
         views: "(222K)",
         value : "0.325 ETH"
     },
     {
+        averta: ellipse3,
         name: "Proof-of-Change",
         views: "(35K)",
         value : "0.908 ETH"
     },
     {
+        averta: ellipse4,
         name: "BigFoot Town",
         views: "(666K)",
         value : "0.309 ETH"
     },
     {
+        averta: ellipse5,
         name: "Reugged Revenants Club",
         views: "(200K)",
         value : "1.43 ETH"
     },
     {
+        averta: ellipse6,
         name: "NFTLeaguez",
         views: "(1.13M)",
         value : "1.34 ETH"
     },
     {
+        averta: ellipse7,
         name: "Lonely Listeners",
         views: "(191K)",
         value : "0.508 ETH"
     },
     {
+        averta: ellipse8,
         name: "Infinite Drive - Car Race",
         views: "(5K)",
         value : "23.34 ETH"
     },
     {
+        averta: ellipse9,
         name: "Women of Mars",
         views: "(176K)",
         value : "0.45 ETH"
     },
     {
+        averta: ellipse10,
         name: "Wolfies",
         views: "(409K)",
         value : "1.234 ETH"
