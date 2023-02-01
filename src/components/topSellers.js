8be2a0d331f0cc4f37bdfef6409df83903002f20
Victor Yohanna <victoryohanna@gmail.com>
2023-02-01 06:16:41 +0100

    add averta to top sellers component

--- src/components/topSellers.js
+++ src/components/topSellers.js
@@ -1,42 +1,44 @@
+
+import { ellipse8, ellipse9, ellipse10, ellipse11, ellipse12, ellipse15, ellipse14,} from "./index";
 const NFTsTopSellers = [
   {
-    image: "",
+    averta: ellipse8,
     name: "OxdO5e42nhsw9",
     value: "0.98ETH",
     status: "31k NFTs Sold",
   },
   {
-    image: "",
+    averta: ellipse9,
     name: "Ella Mae",
     value: "0.98ETH",
     status: "53k NFTs Sold",
   },
   {
-    image: "",
+    averta: ellipse10,
     name: "2j46hai...",
     value: "0.98ETH",
     status: "300k NFTs Sold",
   },
   {
-    image: "",
+    averta: ellipse11,
     name: "Elena Kyle",
     value: "0.98ETH",
     status: "3.1k NFTs Sold",
   },
   {
-    image: "",
+    averta: ellipse12,
     name: "Amandah Adams",
     value: "0.98ETH",
     status: "3.13k NFTs Sold",
   },
   {
-    image: "",
+    averta: ellipse14,
     name: "Oa8bh2nk...",
     value: "0.98ETH",
     status: "3.13k NFTs Sold",
   },
   {
-    image: "",
+    averta: ellipse15,
     name: "Mae Jackson",
     value: "0.98ETH",
     status: "3.13k NFTs Sold",
