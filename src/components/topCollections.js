2ef2581cf3de665c644701d62c8dfd3ee0190a12
Victor Yohanna <victoryohanna@gmail.com>
2023-02-01 06:07:12 +0100

    add averta to top collections component

--- src/components/topCollections.js
+++ src/components/topCollections.js
@@ -1,57 +1,63 @@
-
-import { averta1, averta2 } from "./index";
+import {
+  ellipse1,
+  ellipse2,
+  ellipse3,
+  ellipse4,
+  ellipse5,
+  ellipse6,
+  ellipse7,
+} from "./index";
 
 const NFTsTopCollections = [
-    {
-        averta: averta1,
-        name: "Onigiri Pepes",
-        value: "1654.45ETH",
-        qnty: 0.20,
-        figure: "+81.93"
-    },
-    {
-        image: "",
-        name: "Mutant Ape Yatch Club",
-        value: "4567.45ETH",
-        qnty: 13.50,
-        figure: "+2"
-    },
-    {
-        image: "",
-        name: "BliensNFT",
-        value: "2344.55ETH",
-        qnty: 0.65,
-        figure: "+101.22"
-    },
-    {
-        image: "",
-        name: "Rare Apepe YC",
-        value: "34.34ETH",
-        qnty: 12.30,
-        figure: "+55.56"
-    },
-    {
-        image: "",
-        name: "moodlies by Hanuka",
-        value: "5432.34ETH",
-        qnty: 0.40,
-        figure: "+81.93"
-    },
-    {
-        image: "",
-        name: "DigiDaigaku",
-        value: "9845.22ETH",
-        qnty: 1.20,
-        figure: "+97.89"
-    },
-    {
-        image: "",
-        name: "CLONEX-XTAKASHI...",
-        value: "123.34ETH",
-        qnty: 55.55,
-        figure: "-45.56"
-    },
-    
-]
+  {
+    averta: ellipse1,
+    name: "Onigiri Pepes",
+    value: "1654.45ETH",
+    qnty: 0.2,
+    figure: "+81.93",
+  },
+  {
+    averta: ellipse2,
+    name: "Mutant Ape Yatch Club",
+    value: "4567.45ETH",
+    qnty: 13.5,
+    figure: "+2",
+  },
+  {
+    averta: ellipse3,
+    name: "BliensNFT",
+    value: "2344.55ETH",
+    qnty: 0.65,
+    figure: "+101.22",
+  },
+  {
+    averta: ellipse4,
+    name: "Rare Apepe YC",
+    value: "34.34ETH",
+    qnty: 12.3,
+    figure: "+55.56",
+  },
+  {
+    averta: ellipse5,
+    name: "moodlies by Hanuka",
+    value: "5432.34ETH",
+    qnty: 0.4,
+    figure: "+81.93",
+  },
+  {
+    averta: ellipse6,
+    name: "DigiDaigaku",
+    value: "9845.22ETH",
+    qnty: 1.2,
+    figure: "+97.89",
+  },
+  {
+    averta: ellipse7,
+    name: "CLONEX-XTAKASHI...",
+    value: "123.34ETH",
+    qnty: 55.55,
+    figure: "-45.56",
+  },
+];
 
 export default NFTsTopCollections;
