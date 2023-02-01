f1ace29ec384276206eb656495ad264723679827
Victor Yohanna <victoryohanna@gmail.com>
2023-02-01 15:59:15 +0100

    add animation to trendings

--- src/components/Auctions.jsx
+++ src/components/Auctions.jsx
@@ -1,5 +1,15 @@
 import "../styles/components/auctions.css";
 
+import slide9 from '../assets/images/slide9.png';
+import slide10 from '../assets/images/slide10.png';
+import slide11 from '../assets/images/slide11.png';
+import slide12 from '../assets/images/slide12.png';
+
+import slide1 from '../assets/images/slide1.png';
+import slide2 from '../assets/images/slide2.jpg';
+import slide3 from '../assets/images/slide3.jpg';
+import slide4 from '../assets/images/slide4.jpg';
+
 const Auctions = () => {
   return (
     <div className="auctions-container">
@@ -12,18 +22,35 @@ const Auctions = () => {
         </div>
       </div>
       <div className="auction-img1">
-        <div className="img1"></div>
-        <div className="img2"></div>
-        <div className="img3"></div>
-        <div className="img4"></div>
-        <div className="img5"></div>
+        <div className="img1" >
+          <img src={slide9} alt="" />
+        </div>
+        <div className="img2">
+          <img src={slide10} alt="" />
+        </div>
+        <div className="img3">
+          <img src={slide11} alt="" />
+        </div>
+        <div className="img4">
+          <img src={slide12} alt="" />
+        </div>
+        {/* <div className="img5"></div> */}
       </div>
       <div className="auction-img2">
-        <div className="img1"></div>
-        <div className="img2"></div>
-        <div className="img3"></div>
-        <div className="img4"></div>
-        <div className="img5"></div>
+
+        <div className="img1">
+          <img src={slide1} alt="" />
+        </div>
+        <div className="img2">
+          <img src={slide2} alt="" />
+        </div>
+        <div className="img3">
+          <img src={slide3} alt="" />
+        </div>
+        <div className="img4">
+          <img src={slide4} alt="" />
+        </div>
+        {/* <div className="img5"></div> */}
       </div>
     </div>
   );
--- src/components/Trending.jsx
+++ src/components/Trending.jsx
@@ -1,6 +1,16 @@
 
 import "../styles/components/trending.css";
 
+import slide1 from '../assets/images/slide1.png';
+import slide2 from '../assets/images/slide2.jpg';
+import slide3 from '../assets/images/slide3.jpg';
+import slide4 from '../assets/images/slide4.jpg';
+
+import slide5 from '../assets/images/slide5.jpg';
+import slide6 from '../assets/images/slide6.jpg';
+import slide7 from '../assets/images/slide7.png';
+import slide8 from '../assets/images/slide8.jpg';
+
 const Trending = () => {
   return (
     <div className="trending-container">
@@ -12,20 +22,37 @@ const Trending = () => {
           <button type="button">View All</button>
         </div>
       </div>
-
-      <div className="trend-img1">
-        <div className="img1"></div>
-        <div className="img2"></div>
-        <div className="img3"></div>
-        <div className="img4"></div>
-        <div className="img5"></div>
+      <div className="trend-img1 slide-left">
+        <div className="img1">
+          <img src={slide1} alt="" />
+        </div>
+        <div className="img2">
+          <img src={slide2} alt="" />
+        </div>
+        <div className="img3">
+          <img src={slide3} alt="" />
+        </div>
+        <div className="img4">
+          <img src={slide4} alt="" />
+        </div>
+        {/* <div className="img5"></div> */}
       </div>
-      <div className="trend-img2">
-        <div className="img1"></div>
-        <div className="img2"></div>
-        <div className="img3"></div>
-        <div className="img4"></div>
-        <div className="img5"></div>
+      <div className="trend-img2 slide-right">
+        {/* <div className="img1">
+          <img src="" alt=""></img>
+        </div> */}
+        <div className="img2">
+          <img src={slide5} alt="" />
+        </div>
+        <div className="img3">
+          <img src={slide6} alt="" />
+        </div>
+        <div className="img4">
+          <img src={slide7} alt="" />
+        </div>
+        <div className="img5">
+          <img src={slide8} alt="" />
+        </div>
       </div>
     </div>
   );
