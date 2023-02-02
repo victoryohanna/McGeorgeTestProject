c64a4da5d1ab248b75f1b230ae702a6874b19dc1
Victor Yohanna <victoryohanna@gmail.com>
2023-02-02 08:58:21 +0100

    styled auction  component

--- src/components/Auctions.jsx
+++ src/components/Auctions.jsx
@@ -1,28 +1,31 @@
 import "../styles/components/auctions.css";
+import { IoFlameSharp } from "react-icons/io5";
 
-import slide9 from '../assets/images/slide9.png';
-import slide10 from '../assets/images/slide10.png';
-import slide11 from '../assets/images/slide11.png';
-import slide12 from '../assets/images/slide12.png';
-
-import slide1 from '../assets/images/slide1.png';
-import slide2 from '../assets/images/slide2.jpg';
-import slide3 from '../assets/images/slide3.jpg';
-import slide4 from '../assets/images/slide4.jpg';
+import {
+  slide1,
+  slide2,
+  slide3,
+  slide4,
+  slide9,
+  slide10,
+  slide11,
+  slide12,
+} from "./index";
 
 const Auctions = () => {
   return (
     <div className="auctions-container">
       <div className="auction-caption">
         <div className="auction-text">
-          <h2>Live Auctions</h2>
+          <h1>Live Auctions</h1>
+          <h1 className="lame"><IoFlameSharp/></h1>
         </div>
         <div className="auction-button">
           <button>View All</button>
         </div>
       </div>
       <div className="auction-img1">
-        <div className="img1" >
+        <div className="img1">
           <img src={slide9} alt="" />
         </div>
         <div className="img2">
@@ -37,7 +40,6 @@ const Auctions = () => {
         {/* <div className="img5"></div> */}
       </div>
       <div className="auction-img2">
-
         <div className="img1">
           <img src={slide1} alt="" />
         </div>
