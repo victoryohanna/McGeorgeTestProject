78dae135e86945f2aa5121ea858b13c1d53ce6a5
Victor Yohanna <victoryohanna@gmail.com>
2023-02-02 09:00:22 +0100

    styled collections component

--- src/components/Collections.jsx
+++ src/components/Collections.jsx
@@ -1,8 +1,8 @@
 import "../styles/components/collections.css";
 import { SiDatabricks, SiAddthis } from "react-icons/si";
 import { IoFlameSharp } from "react-icons/io5";
-import NFTsTopCollections from './topCollections';
-import NFTsTopSellers from './topSellers';
+import NFTsTopCollections from "./topCollections";
+import NFTsTopSellers from "./topSellers";
 
 const Collections = () => {
   const topCollections = NFTsTopCollections;
@@ -15,32 +15,30 @@ const Collections = () => {
           <span>24hrs Volume</span>
         </div>
         <div className="collections">
-          {
-            topCollections.map((item, index)=>{
-              return(
-                <div className="collction-items" key={index}>
-            <div className="averta-item1">
-              <div className="averta">
-                <img src={item.averta} alt="" />
+          {topCollections.map((item, index) => {
+            return (
+              <div className="collction-items" key={index}>
+                <div className="averta-item1">
+                  <div className="averta">
+                    <img src={item.averta} alt="" />
+                  </div>
+                  <div className="averta-desc">
+                    <span>{item.name}</span>
+                    <p>{item.value}</p>
+                  </div>
+                </div>
+                <div className="averta-item2">
+                  <div className="databricks">
+                    <SiDatabricks />
+                  </div>
+                  <span>{item.qnty}</span>
+                </div>
+                <div className="averta-item3">
+                  <span>{item.figure}%</span>
+                </div>
               </div>
-              <div>
-                <span>{item.name}</span>
-                <p>{item.value}</p>
-              </div>
-            </div>
-            <div className="averta-item2">
-              <div className="databricks">
-                <SiDatabricks />
-              </div>
-              <span>{item.qnty}</span>
-            </div>
-            <div className="averta-item3">
-              <span>{item.figure}%</span>
-            </div>
-          </div>
-              )
-            })
-          }
+            );
+          })}
           <button className="btn-explor">Explore</button>
         </div>
       </div>
@@ -50,35 +48,33 @@ const Collections = () => {
           <span>Ranking</span>
         </div>
         <div className="sellers">
-          {
-            topSellers.map((item, index)=>{
-              return(
-                <div className="collction-items" key={index}>
-            <div className="averta-item1">
-              <div className="averta">
-                <img src={item.averta} alt="" />
+          {topSellers.map((item, index) => {
+            return (
+              <div className="collction-items" key={index}>
+                <div className="averta-item1">
+                  <div className="averta">
+                    <img src={item.averta} alt="" />
+                  </div>
+                  <div className="averta-desc">
+                    <span>{item.name}</span>
+                    <p>{item.value}</p>
+                  </div>
+                </div>
+                <div className="averta-item2">
+                  <div className="ioFlame">
+                    <IoFlameSharp />
+                  </div>
+                  <span>{item.status}</span>
+                </div>
+                <div className="averta-item3" id="addSign">
+                  <span id="add">
+                    <SiAddthis />
+                  </span>
+                </div>
               </div>
-              <div>
-                <span>{item.name}</span>
-                <p>{item.value}</p>
-              </div>
-            </div>
-            <div className="averta-item2">
-              <div className="databricks">
-                <IoFlameSharp />
-              </div>
-              <span>{item.status}</span>
-            </div>
-            <div className="averta-item3">
-              <span>
-                <SiAddthis />
-              </span>
-            </div>
-          </div>
-              )
-            })
-          }
-          
+            );
+          })}
+
           <button className="btn-discover">Discover</button>
         </div>
       </div>
