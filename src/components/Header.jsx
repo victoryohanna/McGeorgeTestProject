a79c74a48561060259c09a67dfa4598cb7ac148f
Victor Yohanna <victoryohanna@gmail.com>
2023-02-02 09:59:12 +0100

    add pagination

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,9 @@
 import "../styles/components/header.css";
 import { FaTwitter, FaAndroid } from "react-icons/fa";
-import {RxEyeOpen} from 'react-icons/rx';
-import {SiVuetify} from 'react-icons/si';
-import {HiArrowLongRight, HiArrowLongLeft} from 'react-icons/hi2';
-import image1 from '../assets/images/Rectangle1.png'; 
+import { RxEyeOpen } from "react-icons/rx";
+import { SiVuetify } from "react-icons/si";
+import { HiArrowLongRight, HiArrowLongLeft } from "react-icons/hi2";
+import image1 from "../assets/images/Rectangle1.png";
 
 const Header = () => {
   return (
@@ -14,7 +14,7 @@ const Header = () => {
             <span>
               <FaTwitter />
             </span>
-            <span> 
+            <span>
               <FaAndroid />
             </span>
           </div>
@@ -29,31 +29,50 @@ const Header = () => {
       <div className="right-box">
         <div className="img-section">
           <div className="img-container">
-          <img src={image1} alt="nft" />
+            <img src={image1} alt="nft" />
           </div>
           <div className="info-views">
-            <div className="rx-eye"><RxEyeOpen/><span> (450K)</span></div>
-            <div className="si-vue"><SiVuetify/> <span>0.908 ETH</span></div>
+            <div className="rx-eye">
+              <RxEyeOpen />
+              <span> (450K)</span>
+            </div>
+            <div className="si-vue">
+              <SiVuetify /> <span>0.908 ETH</span>
+            </div>
           </div>
           <div className="img-text">
             <h3>Project Woof</h3>
-            <p>The world's first NFT for dogs. Created as a medium for dogs
-              to feel human wothout the pressure of society
+            <p>
+              The world's first NFT for dogs. Created as a medium for dogs to
+              feel human wothout the pressure of society
             </p>
           </div>
         </div>
         <div className="nav-section">
-          <div className="toogle">
-            <div className="c1"></div>
-          </div>
-          <div className="right-arrow">
-            {/* <button><HiArrowLongRight/></button> */}
-          </div>
-          <div className="left-arrow">
-            {/* <button><HiArrowLongLeft/></button> */}
-          </div>
+          
+            <ul className="pagination">
+              
+              <li className="page1">
+                <span >1 </span>
+              </li>
+              <li className="page2">
+                <span >2 </span>
+              </li>
+              <li className="page3">
+                <span >3 </span>
+              </li>
+              <li className="page4">
+                <span >4 </span>
+              </li>
+              
+            </ul>
+
+            <div className="nav-button">
+              <button type="button">Previous</button>
+              <button type="button">Next</button>
+            </div>
+         
         </div>
-        
       </div>
     </div>
   );
